d2bfd8128b916cc00542684cfb13b1f8513f0fb2
prithdamyth <pvelpur@purdue.edu>
2020-05-15 00:08:20 -0400

    First commit - Website in progress

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Where all the css js and img files go
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res) =>{
+    res.render('index', {})
+})
+
+app.get('/about', (req, res) =>{
+    res.render('about', {})
+})
+
+app.get('/experience', (req, res) =>{
+    res.render('experience', {})
+})
+
+
+app.listen(port, () => {
+    console.log('Server started up on port ' + port)
+})
